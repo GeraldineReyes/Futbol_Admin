f813b9e4abcc622a203c5bc808b23553f321c542
JeissonLi <jeissonlinares966@gmail.com>
2021-10-09 17:13:01 -0500

    Capa_Presentacion_Corregida
    Genero en repositorio, Prueba

--- DESARROLLO/proyecto_soccer_manager/ProyectoSoccerManager.App/ProyectoSoccerManager.App.Persistencia/bin/AppRepositorios/IRepositorioSoccerManager.cs
+++ DESARROLLO/proyecto_soccer_manager/ProyectoSoccerManager.App/ProyectoSoccerManager.App.Persistencia/bin/AppRepositorios/IRepositorioSoccerManager.cs
@@ -1,7 +1,15 @@
+using System;
+using ProyectoSoccerManager.App.Dominio;
 namespace ProyectoSoccerManager.App.Persistencia.bin.AppRepositorios
 {
     public interface IRepositorioSoccerManager
     {
-         
+        IEnumerable<Genero> GetAll();
+        IEnumerable<Genero> GetGeneroPorFiltro(string filtro);
+
+        Genero GetGeneroPorId(int GeneroId);
+        Genero Update(Genero generoActualizado);
+
+        Genero Add(Genero nuevoGenero);
     }
 }
--- DESARROLLO/proyecto_soccer_manager/ProyectoSoccerManager.App/ProyectoSoccerManager.App.Persistencia/bin/AppRepositorios/RepositorioSoccerManager.cs
+++ DESARROLLO/proyecto_soccer_manager/ProyectoSoccerManager.App/ProyectoSoccerManager.App.Persistencia/bin/AppRepositorios/RepositorioSoccerManager.cs
@@ -1,7 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProyectoSoccerManager.App.Dominio;
 namespace ProyectoSoccerManager.App.Persistencia.bin.AppRepositorios
 {
-    public class RepositorioSoccerManager
+    public class RepositorioSoccerManager : IRepositorioSoccerManager
     {
+        public RepositorioSoccerManager()
+        {
+        }
+            public Genero Add(Genero nuevoGenero) // Adicionar
+            {
+                nuevoGenero.Id = Generos.Max(r => r.id) + 1; //id: contados Max obtiene el numero mayor, y le suma uno
+                Generos.Add(nuevoGenero);
+                return nuevoGenero;
+            }
+
+            public IEnumerable<Genero> GetAll() //Listar
+            {
+                return Generos;
+            }
+
+            public Genero GetGeneroPorId(int GeneroId)  // Buscar
+            {
+                return Generos.SingleOrDefault(g => g.Id == GeneroId);//recorre la lista de saludos, hasta encontrar uno igual
+            }
+
+            public IEnumerable<Genero> GetGenerosPorFiltro(string filtro) //Desplegable, Falta implementar
+            {
+                throw new NotImplementedException();
+            }
+
+            public Genero Update(Genero generoActualizado)  // Actualizar
+            {
+                var genero = Generos.SingleOrDefault(r => r.id == generoActualizado.Id);
+                if (genero != null)
+                {
+                    genero.Nombre = generoActualizado.Nombre;
+                    genero.Abreviatura = generoActualizado.Abreviatura;
+                }
+                return genero;
+            }
         
+
     }
 }
